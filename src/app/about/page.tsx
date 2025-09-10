04df57d02519b6c544d930c465b32ac540ab674e
Jcgregory <jcgregory734@gmail.com>
2025-09-10 21:07:55 +0530

    Fix: escaped apostrophes for Vercel build

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,14 +4,14 @@ export default function AboutPage() {
         <div className="max-w-2xl mx-auto">
           <h1 className="text-3xl font-bold mb-4 text-gray-900 dark:text-white font-vera-bold text-center">About Me</h1>
           <p className="text-gray-700 dark:text-gray-300 leading-relaxed font-vera-bold mb-6">
-            I'm passionate about Photoshop and love experimenting with new design ideas.
+            I&apos;m passionate about Photoshop and love experimenting with new design ideas.
             This portfolio is a collection of my works over the years.
           </p>
           <p className="text-gray-700 dark:text-gray-300 leading-relaxed font-vera-bold mb-6">
             I have been expanding my expertise to include After Effects, Premiere Pro, Illustrator and Blender(still a Beginner!).
           </p>
           <p className="text-gray-700 dark:text-gray-300 leading-relaxed font-vera-bold mb-6">
-          Though my experience so far comes from personal projects, I’m eager to apply my creativity on a larger, more professional scale.
+          Though my experience so far comes from personal projects, I&apos;m eager to apply my creativity on a larger, more professional scale.
           </p>
           <p className="text-gray-700 dark:text-gray-300 leading-relaxed font-vera-bold">
              Want to connect? My details are available on the Contact page.
